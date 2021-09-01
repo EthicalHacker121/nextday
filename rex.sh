06500f3d12dc10a5c55ce8689a28a286ec5d6dd0
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-09-01 15:50:27 +0530

    Create rex.sh

---
+++ rex.sh
@@ -0,0 +1,8 @@
+apt update
+pkg install git
+pkg install python 
+pkg install figlet
+rm trial* hi*
+wget -q "https://raw.githubusercontent.com/EthicalHacker121/nextday/main/hi.py"
+clear
+python hi.py
